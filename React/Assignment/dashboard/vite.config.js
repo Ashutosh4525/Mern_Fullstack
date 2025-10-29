a216772af53afd4c344f68084770c1b2ebb7b487
Ashutosh4525 <ashutoshpal47@gmail.com>
2025-10-29 20:08:58 +0530

    dashboard_update1

--- React/Assignment/dashboard/vite.config.js
+++ React/Assignment/dashboard/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/React/Assignment/dashboard/",
+  base:"Mern_Fullstack/React/Assignment/dashboard",
   plugins: [react()],
 })

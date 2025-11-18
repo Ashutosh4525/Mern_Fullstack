f16f6e857fa9ae7013555c059fcf4e7aff95213f
Ashutosh4525 <ashutoshpal47@gmail.com>
2025-11-18 23:10:39 +0530

    creating_home_page

--- React/Assignment/puneriweb/src/components/header/Header.jsx
+++ React/Assignment/puneriweb/src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -16,92 +17,74 @@ export default function Header() {
 
   return (
     <div className='container w-full relative'>
-      <header className="fixed bg-gray-800 max-w-9/10 z-50 h-16 flex items-center top-10 parent-skew p-4">
-        <div className='child-skew w-full py-4'>
-        <nav aria-label="Global" className="w-full flex items-center justify-between p-6 lg:px-8 child-skew">
-          <div className="flex lg:flex-1">
-            <a href="/player" className="-m-1.5 p-1.5 ">
-              <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src={logo}
-                className="skew-x-15 translate-y-5"
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-white">
-                {item.name}
-              </a>
-            ))}
-          </div>
-          {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm/6 font-semibold text-white">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div> */}
-        </nav>
-        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-          {/* <div className="fixed inset-0 z-50 w-full" /> */}
-          <div className='w-full'>
-          <DialogPanel className="fixed inset-y-5 right-12 z-50 overflow-y-auto bg-gray-900 p-6">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+      <header className="fixed max-w-9/10 z-50 h-16 w-full flex items-center top-10 parent-skew px-0 py-0">
+        <div className='child-skew w-full flex items-center'>
+          <nav aria-label="Global" className="w-full flex items-center justify-between lg:justify-center p-4 lg:px-8 child-skew">
+            <div className="flex items-center">
+              <a href="/player" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <img
                   alt=""
-                  src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                  className="h-8 w-auto"
+                  src={logo}
+                  className="child-skew skew-x-15 sm:skew-x-0"
                 />
               </a>
+            </div>
+            <div className="hidden lg:flex flex-1 justify-center items-center gap-x-12">
+              {navigation.map((item) => (
+                <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-white">
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <div className="flex lg:hidden flex-1 justify-end">
               <button
                 type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-200"
+                onClick={() => setMobileMenuOpen(true)}
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
               >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon aria-hidden="true" className="size-6 skew-x-15" />
               </button>
             </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-white/10">
-                <div className="space-y-2 py-6">
+          </nav>
+
+          
+          <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+            <div className='fixed inset-0 z-50 flex justify-end'>
+              <DialogPanel className="fixed max-w-9/10 translate-x-4 top-26 inset-x-0 z-50 bg-gray-900 p-6 shadow-lg border-t border-gray-700">
+               
+                <div className="flex items-center justify-end mb-4">
+                  {/* <a href="/" className="-m-1.5 p-1.5">
+                    <span className="sr-only">Your Company</span>
+                    <img src={logo} alt="Logo" className="h-8 w-auto" />
+                  </a> */}
+                  <button
+                    type="button"
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="rounded-md p-2.5 text-gray-200"
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon aria-hidden="true" className="size-6" />
+                  </button>
+                </div>
+               
+                <nav>
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                      className="block rounded-lg px-3 py-2 text-base font-semibold text-white hover:bg-white/5"
                     >
                       {item.name}
                     </a>
                   ))}
-                </div>
-                {/* <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
-                  >
-                    Log in
-                  </a>
-                </div> */}
-              </div>
+                </nav>
+              </DialogPanel>
             </div>
-          </DialogPanel>
-          </div>
-        </Dialog>
+          </Dialog>
         </div>
       </header>
-     </div>
+    </div>
   )
 }
--- React/Assignment/puneriweb/src/components/pages/Hero.jsx
+++ React/Assignment/puneriweb/src/components/pages/Hero.jsx
@@ -1,4 +1,4 @@
-import img1 from "../../assets/banner-title.png";
+import img1 from "../../assets/homepage-mobile-banner-s12.webp";
 
 export default function Hero(){
     return(
@@ -6,7 +6,7 @@ export default function Hero(){
         <div className="hero">  
             <div className="col-md-12 col-xs-12 col-sm-12 no-padd">
                 <img src={img1} alt="banner title" className="hero-img"/>
-                <h1>fixtures &amp; schedule</h1>
+                
             </div>
 
         </div>

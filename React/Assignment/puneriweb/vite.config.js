7118912488075af44c8127f72e06f2c84e7de1e0
Ashutosh4525 <ashutoshpal47@gmail.com>
2025-11-30 20:30:09 +0530

    new_for_git_pages

--- React/Assignment/puneriweb/vite.config.js
+++ React/Assignment/puneriweb/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/puneriweb/",
+  base: "Mern_Fullstack/React/Assignment/puneriweb/",
   plugins: [react(),tailwindcss()],
 })

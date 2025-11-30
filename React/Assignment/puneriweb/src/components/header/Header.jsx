7d5a19b428bbbb5a7a2733c0796c1612733e325a
Ashutosh4525 <ashutoshpal47@gmail.com>
2025-11-30 22:59:05 +0530

    Header_phone

--- React/Assignment/puneriweb/src/components/header/Header.jsx
+++ React/Assignment/puneriweb/src/components/header/Header.jsx
@@ -20,23 +20,39 @@ export default function Header() {
 
   /* Detect scroll direction */
   useEffect(() => {
-    function controlHeader() {
-      if (window.scrollY > lastScrollY) {
-        setShowHeader(false);    // scroll down → hide
-      } else {
-        setShowHeader(true);     // scroll up → show
-      }
-      setLastScrollY(window.scrollY);
+    // function controlHeader() {
+    //   if (window.scrollY > lastScrollY) {
+    //     setShowHeader(false);    // scroll down → hide
+    //   } else {
+    //     setShowHeader(true);     // scroll up → show
+    //   }
+    //   setLastScrollY(window.scrollY);
+    // }
+
+    // window.addEventListener("scroll", controlHeader);
+    // return () => window.removeEventListener("scroll", controlHeader);
+     let lastY = window.scrollY;
+
+  const handleScroll = () => {
+    const currentY = window.scrollY;
+    if (currentY > lastY && currentY > 20) {
+      setShowHeader(false);
+    } 
+    else if (currentY < lastY) {
+      setShowHeader(true);
     }
 
-    window.addEventListener("scroll", controlHeader);
-    return () => window.removeEventListener("scroll", controlHeader);
-  }, [lastScrollY]);
+    lastY = currentY;
+  };
+
+  window.addEventListener("scroll", handleScroll, { passive: true });
+  return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className='container w-full relative'>
       <header className={`fixed max-w-9/10 z-50 h-16 flex items-center top-10 parent-skew px-0 py-0 lg:h-10 transition-all duration-500 ease-in-out 
-         ${showHeader ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-full"}`}>
+        ${showHeader ? "translate-y-0" : "-translate-y-full"}`}>
         <div className='child-skew w-full flex items-center'>
           <nav aria-label="Global" className="w-full flex items-center justify-between lg:justify-center p-4 lg:px-8 child-skew">
             <div className="flex items-center">

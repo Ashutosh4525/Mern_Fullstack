bb8ea1effd9d36a495e3f69edc86e567a37a7533
Ashutosh4525 <ashutoshpal47@gmail.com>
2026-01-15 23:13:55 +0530

    Session3_Node_Mongo

---
+++ Node/Session/session3/app1.js
@@ -0,0 +1,6 @@
+
+const data="this is from app1"
+
+export const val=666654642;
+
+export default data;
---
+++ Node/Session/session3/index.js
@@ -0,0 +1,11 @@
+//Normal import 
+// import data from './app1';
+
+// this is form new name
+import data1 from './app1';
+
+// this iss for new name if not default import
+import { val as value} from './app1';
+
+console.log(data1);
+console.log(value);

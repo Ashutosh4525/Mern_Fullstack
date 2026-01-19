802ad80f686706701848254c9031278c4c525886
Ashutosh4525 <ashutoshpal47@gmail.com>
2026-01-19 21:37:06 +0530

    session5_crud_opreation

--- Node/Session/session4/controller/user.controller.js
+++ Node/Session/session4/controller/user.controller.js
@@ -31,3 +31,142 @@ export const CreateUser= async(req,res)=>{
         
     }
 }
+
+
+export const GetUser=async (req,res) => {
+    try {
+        const users=await User.find();
+        
+        return res.status(200).json({
+            data: users,
+            message:"User fetched successfully",
+            success:true
+        })
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            message:"Somethhing went wrong",
+            success:false
+        })
+        
+    }
+}
+
+export const GetSingleUser= async (req,res) => {
+     try {
+        const {id}=req.params;
+        const user=await User.findById(id).select("-__v");
+
+        if (!user) {
+            return res.status(400).json({
+            message:"User does not exist",
+            success:false
+        })
+        }
+        
+        return res.status(200).json({
+            data: user,
+            message:"User fetched successfully",
+            success:true
+        })
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            message:"Somethhing went wrong",
+            success:false
+        })
+        
+    }
+}
+
+export const UpdateUser= async (req,res) => {
+    try {
+        const {id}=req.params;
+        const user=await User.findById(id);
+
+        //  const data=req.body;
+        // console.log(data);
+
+        // const {name,email,age}=req.body;
+        // console.log(name,email,age);
+
+
+        if (!user) {
+            return res.status(400).json({
+            message:"User does not exist",
+            success:false
+        })
+        }
+        else{
+        const userdata=req.body;
+        console.log(userdata);
+
+        const {name,email,age}=req.body;
+        // await User.updateOne({id});
+        await User.updateOne({_id:id},{name,email,age});
+
+        console.log(user);
+        }
+        return res.status(201).json({
+            data: user,
+            message:"User Updated successfully",
+            success:true
+        })
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            message:"Somethhing went wrong",
+            success:false
+        })
+        
+    }
+}
+
+export const DeleteUser= async (req,res) => {
+    try {
+        const {id}=req.params;
+        const user=await User.findById(id);
+
+        //  const data=req.body;
+        // console.log(data);
+
+        // const {name,email,age}=req.body;
+        // console.log(name,email,age);
+
+
+        if (!user) {
+            return res.status(400).json({
+            message:"User does not exist",
+            success:false
+        })
+        }
+        // else{
+        // const userdata=req.body;
+        // console.log(userdata);
+
+        // const {name,email,age}=req.body;
+        const del=await User.deleteOne({_id:id});
+        console.log(del);
+        // }
+        return res.status(201).json({
+            // data: user,
+            message:"User deleted successfully",
+            success:true
+        })
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            message:"Somethhing went wrong",
+            success:false
+        })
+        
+    }
+}
--- Node/Session/session4/router/user.router.js
+++ Node/Session/session4/router/user.router.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { CreateUser } from "../controller/user.controller"
+import { CreateUser, GetUser, GetSingleUser, UpdateUser, DeleteUser } from "../controller/user.controller"
 
 const userRouter=express.Router();
 
 userRouter.post("/",CreateUser)
+userRouter.get("/",GetUser)
+userRouter.get("/:id",GetSingleUser);
+userRouter.put("/:id",UpdateUser);
+userRouter.delete("/:id", DeleteUser)
+
 
 export default userRouter;

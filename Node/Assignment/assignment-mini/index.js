6ce791e20927f602372d0d912c16c48d0f08444d
Ashutosh4525 <ashutoshpal47@gmail.com>
2026-02-15 23:02:42 +0530

    changes_cors

--- Node/Assignment/assignment-mini/index.js
+++ Node/Assignment/assignment-mini/index.js
@@ -5,12 +5,18 @@ import userRouter from "./router/user.router";
 import errorHandler from "./middlewares/error.middleware";
 import bookRouter from "./router/book.router";
 import authorRouter from "./router/author.route";
+import cors from "cors";
 dotenv.config();
 
 const PORT=process.env.PORT || 8080;
 const app=express();
 app.use(express.json());
 app.use("/images",express.static("uploads"))
+app.use(express.static("public"))
+app.use(cors({
+  origin:['http://127.0.0.1:5500','http://localhost:5174']
+}))
+
 
 // user
 app.use("/api/v1/users", userRouter);
@@ -21,7 +27,7 @@ app.use("/api/v1/authors", authorRouter)
 app.use(errorHandler);
 connectDB()
 .then(()=>{
-    app.listen(PORT,()=>{
+    app.listen(PORT,'0.0.0.0',()=>{
     console.log("connection to db",PORT);
   })
 })

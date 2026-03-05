a39e18601b10834a50234b3453d368d3080c75da
Ashutosh4525 <ashutoshpal47@gmail.com>
2026-03-05 23:31:14 +0530

    main_addition_db_upload_app

---
+++ Major-Project/backend/DB/db.js
@@ -0,0 +1,19 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+import mongoose from "mongoose";
+
+const url=process.env.MONGODB_URI;
+
+const connectDB=async () => {
+  try {
+        const connectionInstance = await mongoose.connect(`${url}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+};
+
+export default connectDB;
+
+

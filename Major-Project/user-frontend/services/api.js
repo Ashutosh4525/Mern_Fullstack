a5203c46e1916e8e153325ff42db4be01e8b9f5c
Ashutosh4525 <ashutoshpal47@gmail.com>
2026-04-11 01:24:15 +0530

    production_changes_prototype

--- Major-Project/user-frontend/services/api.js
+++ Major-Project/user-frontend/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  process.env.API_URL ||
+  process.env.NEXT_PUBLIC_API_URL ||
   "https://movie-app-4nje.onrender.com/api/v1";
 
 export const API = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true
+  withCredentials: true,
+  timeout: 15000,
 });

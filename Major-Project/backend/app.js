bd3b4ff65b278e7704b64fcf05a28371262e7b0f
Ashutosh4525 <ashutoshpal47@gmail.com>
2026-04-11 01:55:55 +0530

    cors_options_issue_solved

--- Major-Project/backend/app.js
+++ Major-Project/backend/app.js
@@ -35,7 +35,7 @@ app.use(cors({
 }));
 
 // ✅ handle preflight requests
-app.options('*', cors());
+app.options(/.*/, cors());
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
